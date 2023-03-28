8a3ed34eaca9274529c1ab6a5e2134c33b7be35b
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2023-03-29 01:23:03 +0530

    Intial Commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '52c2ec9eddff6a9fe13d550f872e1f66'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
